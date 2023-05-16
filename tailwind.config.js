2c201178315caff56de3816ab5866a6eab92a5db
ppratham0811 <ppratham0811@gmail.com>
2023-05-16 17:57:59 +0530

    added react-csv and order tables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1a1c23", // light background
+        secondary: "#121317", // dark background
+        success: "#0e9f6e",
+        inputBg: "#24262d",
+        danger: "#f00",
+      },
+    },
   },
   plugins: [],
-}
+};
 
+// navbar and header color: #1a1c23
+// backgroundColor: #121317
+// input field bg: #24262d
+// success btn: #0e9f6e
