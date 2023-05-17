1f3f67e87bf9121a251c176fac714a56cce9c94a
ppratham0811 <ppratham0811@gmail.com>
2023-05-18 00:54:43 +0530

    added redux and user slice with persist

---
+++ src/app/store.js
@@ -0,0 +1,32 @@
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./userSlice";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import localStorage from "redux-persist/es/storage";
+import persistStore from "redux-persist/es/persistStore";
+
+const persistConfig = {
+  key: "root",
+  storage: localStorage,
+};
+
+export const persistedReducer = persistReducer(persistConfig, userReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export default persistStore(store);
