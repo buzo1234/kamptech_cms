593c7b7bd49f404c05b780fe922f26610517dbf3
buzo1234 <karandua2002@gmail.com>
2023-05-22 20:53:23 +0530

    added relevant details in table

--- src/components/ProductsView.js
+++ src/components/ProductsView.js
@@ -1,20 +1,52 @@
 import React from 'react';
+import "../styles/toggleSwitch.css";
+import BorderColorSharpIcon from "@mui/icons-material/BorderColorSharp";
+import DeleteOutlineSharpIcon from "@mui/icons-material/DeleteOutlineSharp";
 
-function ProductsView({ allProducts }) {
+function ProductsView({
+  allProducts,
+  setEditId,
+  setShow,
+  show,
+  setDeleteId,
+  setDelShow,
+  delShow,
+}) {
+  const handleEditClick = (prodData) => {
+    console.log(prodData);
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+    setEditId(prodData);
+    setShow(!show);
+  };
+
+  const handleDeleteClick = (id) => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+    setDeleteId(id);
+    setDelShow(!delShow);
+  };
+
   return (
     <>
       <div className='w-full overflow-hidden border border-gray-700 rounded-lg ring-1 ring-black ring-opacity-5 mb-8 bg-gray-900 overflow-x-auto'>
         <table className='w-full whitespace-nowrap'>
           <thead className='text-xs font-semibold tracking-wide text-left uppercase border-b border-gray-700 text-gray-400 bg-gray-800'>
             <tr>
-              <td className='px-4 py-3 w-[1/8]'>Image</td>
-              <td className='px-4 py-3 w-[1/8]'>Product Name</td>
-              <td className='px-4 py-3 w-[1/8]'>Qty</td>
-              <td className='px-4 py-3 w-[1/8]'>Tags</td>
-              <td className='px-4 py-3 w-[1/8]'>Category</td>
-              <td className='px-4 py-3 w-[1/8]'>Published</td>
-              <td className='px-4 py-3 w-[1/8]'>Sale Price</td>
-              <td className='px-4 py-3 w-[1/8]'>Cost Price</td>
+              <td className='px-4 py-3'>Icon</td>
+              <td className='px-4 py-3'>Name</td>
+
+              <td className='px-4 py-3'>Category</td>
+              <td className='px-4 py-3'>Cost Price</td>
+              <td className='px-4 py-3'>Sale Price</td>
+              <td className='px-4 py-3'>Qty</td>
+              <td className='px-4 py-3'>Status</td>
+              <td className='px-4 py-3'>Published</td>
+              <td className='px-4 py-3'>Actions</td>
             </tr>
           </thead>
           <tbody>
@@ -23,16 +55,15 @@ function ProductsView({ allProducts }) {
                 $id,
                 title,
                 images,
-                modelName,
-                modelNumber,
+
                 description,
-                tags,
+
                 quantity,
                 salePrice,
                 category,
                 published,
                 costPrice,
-              }) => (
+              }, index) => (
                 <tr key={$id}>
                   <td className='px-4 py-3 '>
                     <img
@@ -41,13 +72,54 @@ function ProductsView({ allProducts }) {
                     />
                   </td>
                   <td className='px-4 py-3 '>{title}</td>
-                  <td className='px-4 py-3 '>{quantity}</td>
+
                   {/* tags should be mapped */}
-                  <td className='px-4 py-3 '>{tags}</td>
+                  {/* LOL wont need it here */}
+
                   <td className='px-4 py-3 '>{category?.name}</td>
-                  <td className='px-4 py-3 '>{published.toString()}</td>
-                  <td className='px-4 py-3 '>{salePrice}</td>
                   <td className='px-4 py-3 '>{costPrice}</td>
+                  <td className='px-4 py-3 '>{salePrice}</td>
+                  <td className='px-4 py-3 '>{quantity}</td>
+                  <td className='px-4 py-3 '>
+                    {quantity > 0 ? (
+                      <span className='bg-green-700 px-2 py-[0.5] rounded-lg text-sm'>
+                        Selling
+                      </span>
+                    ) : (
+                      <span className='bg-red-700 px-2 py-[0.5] rounded-lg text-sm'>
+                        Sold Out
+                      </span>
+                    )}
+                  </td>
+                  <td className='px-4 py-3 '>
+                    <input
+                      type='checkbox'
+                      checked={published}
+                      className='w-5 h-5 rounded-lg bg-green-600'
+                    />
+                  </td>
+                  <td className="px-4 py-3">
+                      <div className="flex justify-evenly w-full">
+                        <button
+                          className="mr-2"
+                          onClick={() => handleEditClick(allProducts[index])}
+                        >
+                          <BorderColorSharpIcon
+                            className="hover:text-green-600"
+                            style={{ width: "19px", height: "19px" }}
+                          />
+                        </button>
+                        <button
+                          className="mr-2"
+                          onClick={() => handleDeleteClick($id)}
+                        >
+                          <DeleteOutlineSharpIcon
+                            className="hover:text-red-600"
+                            style={{ width: "19px", height: "19px" }}
+                          />
+                        </button>
+                      </div>
+                    </td>
                 </tr>
               )
             )}
